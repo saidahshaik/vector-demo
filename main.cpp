4796f226b8bd91924d2317f1dd7f0510f247bbcc
saidahshaik <saida9000shaik@gmail.com>
2022-08-22 08:58:33 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v1={10,20,30,40,50};
+
+    for(auto n:v1)
+    cout<<n<<" ";
+    cout<<endl;
+    
+    return 0;
+}
